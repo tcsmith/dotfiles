a607f22ae890be5df03a747593566d9febff4563
Todd Smith <todd@dingtech.dev>
2026-09-18 20:13:37 -0400

    ruff for linting and fixes!!! the full astral stack: uv + ty + ruff 💪

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -104,9 +104,26 @@ vim.lsp.enable("powershell_es")
 
 -- Python
 
+-- ruff
+vim.lsp.config("ruff", {
+  cmd = {
+    "ruff",
+    "server",
+  },
+  filetypes = {
+    "python",
+  },
+  root_markers = {
+    "pyproject.toml",
+    "ruff.toml",
+    ".ruff.toml",
+    ".git",
+  },
+})
+
+vim.lsp.enable("ruff")
 
 -- ty
-
 vim.lsp.config("ty", {
   cmd = {
     "ty",
